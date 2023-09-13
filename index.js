4d22e5c93d463d494825e8f9511a587803e4818a
OkkashaAlly <steamnature006@gmail.com>
2023-09-13 07:19:36 +0300

    13 - Module Caching

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-require('./batman')
-require('./superman')
+// const superhero = require('./super-heros')
+
+// console.log(superhero.getName())
+// superhero.setName('superman')
+// console.log(superhero.getName())
+
+// const newSuperhero = require('./super-heros')
+// console.log(newSuperhero.getName())
+
+const SuperHero = require('./super-heros')
+
+const batman = new SuperHero('batman')
+console.log(batman.getName())
+
+const superman = new SuperHero('superman')
+console.log(superman.getName())
+

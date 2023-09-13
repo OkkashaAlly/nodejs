67aef6905e34d3f3854da40837e540e17204f219
OkkashaAlly <steamnature006@gmail.com>
2023-09-13 06:53:16 +0300

    10 - Module Exports

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-require('./add')
+const addFn = require('./add')
 
 console.log("Hello world from index.js")
 
+const sum = addFn(4, 2)
+const sum2 = addFn(4, 4)
+
+console.log(sum)
+console.log(sum2)

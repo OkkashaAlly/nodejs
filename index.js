f7e1c704b776a849c6003f7f202132d8c2262b2a
OkkashaAlly <steamnature006@gmail.com>
2023-09-14 12:04:04 +0300

    19 - Path Module

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-const data = require("./data.json");
+// const path = require('node:path');
+const path = require('path');
 
-console.log(data);
-console.log("Hello");
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.basename(__filename))
+// console.log(path.basename(__dirname))
+
+// console.log(path.extname(__filename))
+
+// console.log(path.dirname(__filename))
+// console.log(path.dirname(__dirname))
+
+// console.log(path.parse(__filename))
+
+// console.log(path.isAbsolute(__filename))
+// console.log(path.isAbsolute('./index.js'))
+
+// console.log(path.join('folder1', 'folder2', 'folder3', 'index.html'))
+// console.log(path.join('/folder1', 'folder2', 'folder3', 'index.html'))
+// console.log(path.join('/folder1', '//folder2', 'folder3', 'index.html'))
+// console.log(path.join('/folder1', '//folder2', 'folder3', '../index.html'))
+// console.log(path.join(__dirname, 'data.json'))
+
+console.log(path.resolve('folder1', 'folder2', 'folder3', 'index.html'))
+console.log(path.resolve('/folder1', 'folder2', 'folder3', 'index.html'))
+console.log(path.resolve('/folder1', '//folder2', 'folder3', 'index.html'))
+console.log(path.resolve('/folder1', '//folder2', 'folder3', '../index.html'))
+console.log(path.resolve(__dirname, 'data.json'))

2444e2dd15c98d9ebe26f3a746ee0750c78f25ee
OkkashaAlly <steamnature006@gmail.com>
2023-09-14 11:38:58 +0300

    16 - ES Modules

--- math.js
+++ math.js
@@ -1,6 +1,9 @@
 // module.exports.add = (a, b) => a + b;
 // module.exports.subtract = (a, b) => a - b;
 
+// exports.add = (a, b) => a + b;
+// exports.subtract = (a, b) => a - b;
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 

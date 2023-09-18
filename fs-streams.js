0c123cad244d708230f38ec15733d8949f5bc363
OkkashaAlly <steamnature006@gmail.com>
2023-09-18 07:04:06 +0300

    29 - Pipes

--- fs-streams.js
+++ fs-streams.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
+const zlib = require('zlib')
+
+const gzip = zlib.createGzip()
 
 const readableStream = fs.createReadStream('./file.txt', {
   encoding: 'utf-8',
@@ -7,7 +10,11 @@ const readableStream = fs.createReadStream('./file.txt', {
 
 const writableStream = fs.createWriteStream('./file2.txt')
 
-readableStream.on('data', (chunk) => {
-  console.log(chunk)
-  writableStream.write(chunk)
-})
+// readableStream.on('data', (chunk) => {
+//   console.log(chunk)
+//   writableStream.write(chunk)
+// })
+
+readableStream.pipe(writableStream)
+
+readableStream.pipe(gzip).pipe(fs.WriteStream('./file3.txt.gz'))

6e9e71858d29804b64446c916f8f9bc834457db6
OkkashaAlly <steamnature006@gmail.com>
2023-09-18 06:49:01 +0300

    27 - fs Promise Module

--- fs-module.js
+++ fs-module.js
@@ -1,23 +1,43 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-// READ DATA 
-// const fileData = fs.readFileSync("./text.txt")
-// console.log(fileData.toString())
+// // READ DATA
+// // const fileData = fs.readFileSync("./text.txt")
+// // console.log(fileData.toString())
 
-const fileData = fs.readFileSync("./text.txt", "utf-8");
-console.log(fileData)
+// const fileData = fs.readFileSync("./text.txt", "utf-8");
+// console.log(fileData)
 
-fs.readFile("./text.txt", "utf-8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// fs.readFile("./text.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
+// // WRITE DATA
+// fs.writeFileSync("./text.txt", "Hello World -");
 
-// WRITE DATA
-fs.writeFileSync("./text.txt", "Hello World -");
+// fs.writeFileSync("./text.txt", "Hello World2 ", { flag: 'a'},(err) => {
+//   if (err) throw err;
+//   console.log("Data written to file");
+// }
+// );
 
-fs.writeFileSync("./text.txt", "Hello World2 ", { flag: 'a'},(err) => {
-  if (err) throw err;
-  console.log("Data written to file");
-}
-);
+const fs = require("fs/promises");
+
+fs.readFile("./text.txt", "utf-8")
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+async function readFile() {
+  try {
+    const data = await fs.readFile("./text.txt", "utf-8");
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}  
+
+readFile();

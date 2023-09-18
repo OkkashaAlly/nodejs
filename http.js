f323b06ffb1f36ddda2dc18541dcd6d72c58b490
OkkashaAlly <steamnature006@gmail.com>
2023-09-18 07:18:44 +0300

    31 - Creating a Node Server

---
+++ http.js
@@ -0,0 +1,20 @@
+const http = require('http')
+
+const server = http.createServer((req, res)=> {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end('<p>Hello World</p>')
+})
+
+// const server = http.createServer((req, res)=> {
+//   const superHero = {
+//     name: 'Batman',
+//     age: 30,
+//     city: 'Gotham'
+//   }
+//   res.writeHead(200, {'Content-Type': 'application/json'})
+//   res.end(JSON.stringify(superHero))
+// })
+
+server.listen(5000, ()=> {
+  console.log('Server running...')
+})

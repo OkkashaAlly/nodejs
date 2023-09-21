1bcef976280b9b98d6b4fcb031e89c3e2533eb33
OkkashaAlly <steamnature006@gmail.com>
2023-09-21 06:40:41 +0300

    59 - CLI Options

--- my-cli-tool/index.js
+++ my-cli-tool/index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('Hello, world!');
+const yargs = require('yargs');
+const {argv} = yargs(process.argv)
+
+
+const print5PokemonMoves = async (pokemonName) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+  const pokemon = await response.json();
+  const moves = pokemon.moves.slice(0, 5).map(m => m.move.name);
+  console.log(moves);
+}
+
+print5PokemonMoves(argv.pokemon);
